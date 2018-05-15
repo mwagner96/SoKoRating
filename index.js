11bf40aa574c6cbda0d1e01c9fe00be2704d56a2
mwagner96 <33757014+mwagner96@users.noreply.github.com>
2018-05-15 14:31:27 +0200

    Baseline

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+
+var ratings = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0
+};
+
+//Call Handler: Was wird bei Aufruf der Seite gemacht
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/test", function(req, res){
+  res.sendFile(__dirname + "/test.html");
+});
+
+app.get("/rate/:rating", function(req, res) {
+  console.log(req.params.rating);
+  ratings[req.params.rating]++;
+  console.log(ratings);
+  res.send("Yay");
+});
+
+app.get("/result", function(req, res){
+  res.sendFile(__dirname + "/result.html");
+})
+
+app.get("/ratings", function(req, res){
+  res.send(JSON.stringify(ratings));
+});
+
+var server = app.listen(3000, function() {
+  console.log("Server läuft =)")
+});
